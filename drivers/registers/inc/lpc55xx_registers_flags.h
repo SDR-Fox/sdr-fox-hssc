6427b2d0c0b417f8079f9aa9399922635bd55a9f
Ludovic-Lesur <ludovic.lesur46@gmail.com>
2025-09-27 10:37:27 +0200

    Add missing flags file.

---
+++ drivers/registers/inc/lpc55xx_registers_flags.h
@@ -0,0 +1,13 @@
+/*
+ * lpc55xx_registers_flags.h
+ *
+ *  Created on: 15 apr. 2025
+ *      Author: Ludo
+ */
+
+#ifndef __LPC55XX_REGISTERS_FLAGS_H__
+#define __LPC55XX_REGISTERS_FLAGS_H__
+
+/*** LPC55xx registers compilation flags ***/
+
+#endif /* __LPC55XX_REGISTERS_FLAGS_H__ */
